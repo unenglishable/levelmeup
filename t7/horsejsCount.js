095b81563c30b411ffb4e1eca4943717624152b5
unenglishable <unenglishable@gmail.com>
2014-07-11 12:02:35 -1000

    original implementation for t7
    uses a single readStream

---
+++ t7/horsejsCount.js
@@ -0,0 +1,15 @@
+module.exports = function (db, date, callback) {
+  var count = 0;
+  var rStream = db.createReadStream({
+    start: date
+  });
+  rStream.on('error', function(err) {
+    callback(err);
+  });
+  rStream.on('data', function(data) {
+    count++;
+  });
+  rStream.on('end', function() {
+    callback(count);
+  });
+}
