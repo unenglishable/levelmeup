ed6e5cf05218f5beb989bc1cc3d9c0f9ed66c9b8
unenglishable <unenglishable@gmail.com>
2014-07-11 19:07:44 -1000

    official solution to t9 using mapped operations
    You can use a map to specify operations on data sets
    for batch operations, the json type can be used to
    specify the operation (put, get, del) and the following
    fields are the actual data to manipulate.
    
    The key can be anything and the value can apparently
    be parsed from the entire json row...  I'm not sure
    how it does this or why that is the functionality.

---
+++ t9/official.js
@@ -0,0 +1,20 @@
+var level = require('level');
+var db = level(process.argv[2], {valueEncoding: 'json'});
+var data = require(process.argv[3]);
+var operations = data.map(function(row) {
+  var key;
+  if (row.type === 'user') {
+    key = row.name;
+  }
+  else if (row.type === 'repo') {
+    key = row.user+'!'+row.name;
+  }
+  else {
+    console.error('Oh shit!');
+  }
+  return {
+    type: 'put', key: key, value: row
+  }
+});
+
+db.batch(operations);
